02b351e3673972004c5f0b6c73488aba73f49e10
Lyz03 <lizoe@netcourrier.com>
2021-12-21 20:01:29 +0100

    exo 130

--- app.js
+++ app.js
@@ -79,3 +79,140 @@ part13()
 
 // part 14
 $('input[type=text]').first().val()
+
+// part 15
+function removeAllClass(element) {
+    element.removeClass()
+}
+
+removeAllClass($('.class1'));
+
+// part 16
+const part16Span = $('#css')
+part16Span.css('color', 'blue');
+part16Span.removeAttr('style');
+
+// part 17
+window.addEventListener('contextmenu', function (event) {
+    event.preventDefault();
+    console.log('click gauche')
+});
+
+$('body').click(function (e) {
+    if (e.button === 0)
+        console.log('click droit')
+})
+
+// part 18
+function checkInteger(nb) {
+    if (Number.isInteger(nb)) {
+        console.log("c'est un entier")
+    }
+}
+
+// part 19
+window.addEventListener("keydown", function (event) {
+    if (event.key === 'Enter')
+        console.log('Enter')
+});
+
+// part 20
+console.log($('tr').length);
+
+// part 21
+console.log($('textArea').val())
+
+// part 22
+$('#form').append('<input type="radio" value="radio">');
+
+// part 23
+console.log($('p').first().offset());
+
+// part 24
+$('p').each(function(index, elem) {
+    elem.style.textTransform = "uppercase";
+})
+
+// part 25
+$('textarea').change(function () {
+    console.log('changé');
+})
+
+// part 26
+$('tr').last().remove();
+
+// part 27
+$('button').text('nouveau text');
+
+// part 28
+$("select").append('<option>opt 1</option>, <option>opt 2</option>');
+
+// part 29
+$("textarea").css("background", "gray");
+
+// part 30
+$('tr').siblings().remove();
+
+// part 31
+$("select").change(function () {
+    console.log($(this).val())
+});
+
+// part 32
+$('a').removeAttr("href");
+
+// part 33
+$('.class')[0].className = "newClass";
+
+// part 34
+$('select').addClass("css");
+
+// part 35
+console.log($('#selected p').length)
+
+// TODO part 36 !!!!!!
+
+// part 37
+console.log(jQuery.fn.jquery);
+
+// part 38
+$('tr').eq(0).remove();
+
+// part 39
+$('input[type=text]').val('new value');
+
+// part 40
+$('span').text('new text');
+
+// part 41
+$('span').click(function () {
+    console.log($(this).attr("class"))
+});
+
+// part 42
+window.addEventListener('load', function () {
+    $('a').attr("href", "newLink")
+});
+
+// part 43
+function deleteDisabled(element) {
+    element.removeAttr("disabled")
+}
+deleteDisabled($('select'));
+
+// part 44
+console.log($('div').first().outerWidth())
+
+// part 45
+$('button').mouseenter(function () {
+    $('button').fadeOut()
+});
+
+// part 46
+let value = [];
+$('#form').children().each(function (index, element) {
+    value.push(element.value)
+});
+
+// part 47
+$('span').first().attr('data-attribute', 'value')
