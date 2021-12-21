3a610f3f9d47ea442c064bfaaa1ce3fbd438dda5
Lyz03 <lizoe@netcourrier.com>
2021-12-21 16:32:50 +0100

    part 14

--- app.js
+++ app.js
@@ -35,6 +35,12 @@ $("a").click(function () {
 $('textarea').attr("maxlength", "30");
 
 // TODO part 7 !!!!
+$('p').each(function(index, elem) {
+    let words = elem.innerText.split(" ");
+    let firstWord = words[0];
+    words.shift();
+    elem.innerHTML = '<b>' + firstWord + ' ' + '</b>' + words.join(" ");
+})
 
 // part 8
 jQuery('body').append('<div style="background-color: gray;color: white">new Div</div>');
@@ -43,6 +49,33 @@ jQuery('body').append('<div style="background-color: gray;color: white">new Div<
 $('#div2').appendTo($('#div1'));
 
 // part 10
+let MyObject = {
+    prop1: "value1",
+    prop2: "value2"
+}
 
+let part10P = $('#part10')
 
+$.each(MyObject, function (property, value) {
+    part10P.text(part10P.text() + property + ' = ' + value + ' ');
+})
 
+// part 11
+$('ul').first().append('<li>item 4</li>');
+
+// part 12
+function part12() {
+    let select = $('select').first()
+    select.children().remove()
+    select.append('<option>option 4</option>')
+}
+part12()
+
+// part 13
+function part13() {
+    $('p').css("text-decoration", "underline")
+}
+part13()
+
+// part 14
+$('input[type=text]').first().val()
